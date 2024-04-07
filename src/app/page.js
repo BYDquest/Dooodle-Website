a5efcee6268fa521a3d0890f7bedefe10d3e0c78
$BYD <bydquest@gmail.com>
2024-04-07 15:21:04 +0800

    update

--- src/app/page.js
+++ src/app/page.js
@@ -15,7 +15,7 @@ export default function Home() {
     // Load more SVGs when reaching near the bottom of the page
     const loadMoreSVGs = () => {
       setLoadedSVGCount((prevCount) => {
-        const newCount = Math.min(prevCount + 16, 100); // Increase by 16, max out at 100
+        const newCount = Math.min(prevCount + 16, 1000); // Increase by 16, max out at 1000
         updateSvgFiles(newCount);
         return newCount;
       });
