8a7bfab6c6fbdce29a8be9275bd076b16b8b9c2a
$BYD <bydquest@gmail.com>
2024-04-07 15:57:05 +0800

    update

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ import React, { useEffect, useState } from 'react';
 export default function Home() {
   const [svgFiles, setSvgFiles] = useState([]);
   const [loadedSVGCount, setLoadedSVGCount] = useState(16); // Start with 16 SVGs
+  const [hoverIndex, setHoverIndex] = useState(null); // Track hover state
 
   useEffect(() => {
     // Function to update the SVG files state
@@ -44,8 +45,25 @@ export default function Home() {
     <div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-0">
         {svgFiles.map((file, index) => (
-          <div key={index} className="flex justify-center items-center p-0">
-            <img src={file} alt={`Avatar ${index}`} className="object-fill m-0 p-0" loading="lazy" />
+          <div
+            key={index}
+            className="flex justify-center items-center p-0 relative"
+            onMouseEnter={() => setHoverIndex(index)}
+            onMouseLeave={() => setHoverIndex(null)}
+          >
+            <img
+              src={file}
+              alt={`Avatar ${index}`}
+              className="object-fill m-0 p-0"
+              loading="lazy"
+            />
+            {hoverIndex === index && (
+              <div
+                className="absolute  inset-0 bg-black bg-opacity-50 flex justify-center items-center text-white text-2xl"
+              >
+                Life time ID: {index}
+              </div>
+            )}
           </div>
         ))}
       </div>
